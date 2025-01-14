a69b95a22ca48923ed331ef994ed7d9a54f02c86
artem3605 <artemabaturov75@gmail.com>
2025-01-14 10:42:27 +0200

    remove unnecessary functions

--- src/answer_generation/prompts.py
+++
@@ -1,8 +0,0 @@
-ASKING_PROMT = """
-You are answering a question from a student at the Neapolis University Paphos who is studying in the program of Computer Science and Artificial Intelligence.
-There is an abbreviation "pg" in the text - it is an abbreviation for Paphos Gardens. This is the place where almost all the students live.
-Find the answer to the question in the text and provide a detailed response.
-It is strictly forbidden to use data in the answer that is not in the text.
-The answer must be in the same language as the question.
-If you did not find the answer in the text, then say that the answer was not found.
-"""
--- src/file_upload_bot/main.py
+++ src/file_upload_bot/main.py
@@ -98,46 +98,6 @@ async def handle_document(message: types.Message):
         await message.reply("You can upload only zip archives, pdf and json files.")
 
 
-@dp.message(Command('show'))
-async def list_files(message: types.Message):
-    if not check_user(message.from_user.id):
-        await message.reply("You are not allowed to use this bot.")
-        return
-
-    file_list = []
-    for root, dirs, files in os.walk(DOWNLOAD_DIR):
-        for name in dirs:
-            file_list.append(os.path.relpath(os.path.join(root, name), DOWNLOAD_DIR) + "\\")
-        for name in files:
-            file_list.append(os.path.relpath(os.path.join(root, name), DOWNLOAD_DIR))
-
-    if not file_list:
-        await message.reply("No files uploaded yet.")
-    else:
-        file_list_str = "\n".join(f"📁 {file}" if file.endswith('\\') else f"📄 {file}" for file in file_list)
-        await message.reply(f"Uploaded files:\n\n{file_list_str}")
-
-
-@dp.message(Command('delete_file'))
-async def delete_file(message: types.Message):
-    if not check_user(message.from_user.id):
-        await message.reply("You are not allowed to use this bot.")
-        return
-
-    command_args = message.text.split(maxsplit=1)
-    if len(command_args) > 1:
-        file_name = command_args[1]
-    else:
-        await message.reply("Please specify the file name to delete.")
-        return
-
-    file_path = os.path.join(DOWNLOAD_DIR, file_name)
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        await message.reply(f"File '{file_name}' deleted successfully.")
-    else:
-        await message.reply(f"File '{file_name}' not found.")
-
 
 FEEDBACK_FILE = Path("feedback.txt")
 
